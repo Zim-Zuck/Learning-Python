e64f02835564fe08cd82deb0e857bc7100a4a6f1
Kunashe <tizkriz69@gmail.com>
2021-10-15 23:16:11 +0200

    Basic Python Programming
    A mixture of if and else statements showing some of the first lines of code you learn when starting python

---
+++ Basics.py
@@ -0,0 +1,13 @@
+saying_hi = "Bonjour. I am Kunashe, I am"
+print(saying_hi * 1)
+age_list = [12, 13, 14, 15]
+print(age_list[1])
+for i in range(1):
+	print(str (i) + ": How are you?")
+are_you_fine = True
+print(are_you_fine) # true
+
+if are_you_fine:
+    print("Yes, I am feeling great")
+else:
+    print("Not really")
